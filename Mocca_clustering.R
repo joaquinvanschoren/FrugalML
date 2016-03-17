bb2918673726ce45382c561551f14dc051d87e8c
mixarus <wmmix@mail.ru>
2016-03-17 13:50:44 +0100

    names convention

--- Mocca_clustering.R
+++ Mocca_clustering.R
@@ -7,9 +7,9 @@ if(newCalculatations) {
     mocca_res <- mocca(hugeMatrix, R = 10, K = 2:10, iter.max = 1000, nstart = 10) 
     analyzePareto(mocca_res$objectiveVals) 
 } else {
-    mocca_res <- readRDS("objects/mocca_res_huge_matrix_complete.rda") 
+    mocca_res <- readRDS("objects/mocca_res_huge_matrix_complete.rds") 
 } 
 
 # save results since it takes substatial amount of time to compute a number of clusters 
-saveRDS(mocca_res, file = "objects/mocca_res_huge_matrix_complete.rda") 
+saveRDS(mocca_res, file = "objects/mocca_res_huge_matrix_complete.rds") 
  
