c913e436c540e3320627f95132bcef2e2fe28668
mixarus <wmmix@mail.ru>
2016-03-17 20:05:39 +0100

    duplicate functions

--- Mocca_clustering.R
+++ Mocca_clustering.R
@@ -6,11 +6,12 @@ newCalculatations = FALSE
 if(newCalculatations) { 
     bSize <- dim(hugeMatrix)[1] 
     mocca_res <- mocca(hugeMatrix, R = bSize, K = 2:30, iter.max = 1000, nstart = 10) 
-    analyzePareto(mocca_res$objectiveVals) 
 } else {
     mocca_res <- readRDS("objects/mocca_res_huge_matrix_complete.rds") 
 } 
 
+analyzePareto(mocca_res$objectiveVals) 
+
 # save results since it takes substatial amount of time to compute a number of clusters 
 saveRDS(mocca_res, file = "objects/mocca_res_huge_matrix_complete.rds") 
- 
+
