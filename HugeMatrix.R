ca068a5b9abc212e563be7b7fe3c32628714982f
mixarus <wmmix@mail.ru>
2016-03-18 13:11:31 +0100

    huge matrix functions

--- HugeMatrix.R
+++ HugeMatrix.R
@@ -5,126 +5,50 @@ setwd("/home/mikhail/Desktop/GitProjects/FrugalML")
  
 # results of algorithms
 evaluations <- read.csv("data/openml_evaluations_all.csv") 
+separate_evaluations <- split(evaluations, evaluations$task_id) 
 
-createHugeMatrix <- function(originData) {  
+source("HugeMatrixFunctions.R") 
 
-    separate_evaluations <- split(originData, originData$task_id)
-    
-    # save names of data sets for further analysis 
-    allMeasures <- data.frame() 
-    
-    # find all algrothms from all results
-    douplicatedAlgos <- originData$algo
-    algorithms <- data.frame(levels(douplicatedAlgos))
-    
-    # prepare matrix with filling in the names of algorithms as column names
-    hugeMatrix <- data.frame(t(algorithms))
-    colnames(hugeMatrix) <- algorithms[1:103, 1]
-    hugeMatrix <- hugeMatrix[-1,] 
-    
-    # store the number of algorithms in all items 
-    numOfAlgs <- dim(hugeMatrix)[2] 
-    
-    # create short names 
-    colnames(algorithms) <- "algorithm" 
-    algorithms$algorithm <- as.character(algorithms$algorithm) 
-    for (i in 1: numOfAlgs) { 
-        algFullName<- as.character(algorithms[i, 1])  
-        
-        posOfEmpty <- gregexpr(pattern =' ', algFullName)[[1]][1] 
-        
-        if (posOfEmpty > 0) {  
-            shortName <- paste(substr(algFullName, 0, 5), substr(algFullName, 23, posOfEmpty - 1), sep = "") 
-        } else {
-            shortName <- paste(substr(algFullName, 0, 5), substr(algFullName, 23, nchar(algFullName)), sep = "") 
-        }
-        
-        algorithms[i, 1] <- shortName 
-    } 
-    algorithms <- data.frame(t(algorithms))   
-    
-    # change from factor to character 
-    algorithms[] <- lapply(algorithms, as.character) 
-    
-    # replace long names with short names 
-    colnames(hugeMatrix) <- algorithms[1, ]  
-    
-    # calculate the number of all sets in files 
-    quantityDataSets <- length(separate_evaluations) 
-    
-    for (i in 1:quantityDataSets) {
-        # make a copy of set
-        x <- as.data.frame(separate_evaluations[i])
-        x <-
-            setNames(
-                x, c(
-                    "task_id", "dataset", "algo", "error_message", "accuracy", "auroc", "training_millis", "testing_millis", "confusion_matrix"
-                )
-            )
-        
-        # receive name 
-        xName <- as.character(x$dataset[1])
-        allMeasures[i, 1] = xName    
-        
-        # change type from factor to numeric and remove NA values 
-        x$auroc <- as.numeric(as.character(x$auroc))
-        x <- x[!is.na(x$auroc),] 
-        
-        # store highest value for AUC in this particular data set  
-        highestAUC <- max(x$auroc) 
-        
-        # convert time variable to numbers 
-        x$training_millis <-
-            as.numeric(as.character(x$training_millis))
-        x$testing_millis <-
-            as.numeric(as.character(x$testing_millis))
-        
-        # check for missing or negative values   
-        x <- x[!is.na(x$training_millis),]
-        x$training_millis <- abs(x$training_millis) 
-        
-        x <- x[!is.na(x$testing_millis),] 
-        x$testing_millis <- abs(x$testing_millis) 
-        
-        # caclulate combined time for each algorithm
-        x$combineTime <- x$training_millis + x$testing_millis 
-        
-        # find the lowest combined time in this data set   
-        lowestTime <- min(x$combineTime + 1) 
-        
-        # filter columns and save AUC, training time and name of algorithm
-        x <- data.frame(x$auroc, x$combineTime, x$algo)
-        smallX <- setNames(x, c("AUC", "CombineTime", "Algorithm"))
-        smallX <-
-            smallX[order(smallX$AUC,smallX$CombineTime,decreasing = TRUE),]
-        
-        # create a vector of values for this set 
-        paretoRes <- t(data.frame(rep(NA, numOfAlgs)))  
-        colnames(paretoRes) <- colnames(hugeMatrix)  
-        rownames(paretoRes) <- xName 
-        
-        # update value for each algorithm and data set       
-        for (algValue in 1:nrow(smallX)) { 
-            algName <- as.character(smallX[algValue, 3])
-            algIndex <- grep(pattern = substr(algName, 0, 5), x = colnames(paretoRes), ignore.case = TRUE) 
-            paretoRes[1, algIndex] <- smallX[algValue, 1] - 0.1 * log(1 + smallX[algValue, 2]) 
-        } 
+# find all algrothms from all results
+algorithms <- data.frame(levels(evaluations$algo)) 
  
-        if (sum(is.na(paretoRes) > 0)) {
-                
-        }
-               
-        # replace all NA values with negative value 
-        paretoRes[is.na(paretoRes)] <- -1  
-        
-        # add results to the matrix    
-        hugeMatrix <- rbind(hugeMatrix, paretoRes)  
-    } 
-    
-    return (hugeMatrix)  
-} 
+# prepare matrix with filling in the names of algorithms as column names
+hugeMatrix <- data.frame(t(algorithms))
+colnames(hugeMatrix) <- algorithms[1:103, 1]
+hugeMatrix <- hugeMatrix[-1,]
 
-hugeMatrix <- createHugeMatrix(evaluations) 
+# store the number of algorithms in all items
+numOfAlgs <- dim(hugeMatrix)[2]
+
+# prepare names of algorithms for processing
+colnames(algorithms) <- "algorithm"
+algorithms$algorithm <- as.character(algorithms$algorithm)
+
+# create short names
+for (i in 1:numOfAlgs) {
+    algFullName <- as.character(algorithms[i, 1])
+    algorithms[i, 1] <- shortNameAlgorithm(algFullName)
+}
+algorithms <- data.frame(t(algorithms))
+
+# change from factor to character
+algorithms[] <- lapply(algorithms, as.character)
+
+# replace long names with short names
+colnames(hugeMatrix) <- algorithms[1,]
+
+# calculate the number of all sets in files
+quantityDataSets <- length(separate_evaluations)
+
+for (i in 1:quantityDataSets) {
+    # make a copy of set
+    dataSet <- as.data.frame(separate_evaluations[i])
+    
+    processedDataSet <- originalScoreData(hugeMatrix, dataSet)
+    
+    # add results to the matrix
+    hugeMatrix <- rbind(hugeMatrix, processedDataSet)
+} 
 
 write.csv(hugeMatrix, "hugeMatrix.csv") 
 
---
+++ HugeMatrixFunctions.R
@@ -0,0 +1,141 @@
+createHugeMatrix <- function(originData, splitFactor) {
+    separate_evaluations <- split(originData, originData[splitFactor])
+    
+    # find all algrothms from all results
+    algorithms <- data.frame(levels(originData$algo))
+    
+    # prepare matrix with filling in the names of algorithms as column names
+    hugeMatrix <- data.frame(t(algorithms))
+    colnames(hugeMatrix) <- algorithms[1:103, 1]
+    hugeMatrix <- hugeMatrix[-1,]
+    
+    # store the number of algorithms in all items
+    numOfAlgs <- dim(hugeMatrix)[2]
+    
+    # prepare names of algorithms for processing
+    colnames(algorithms) <- "algorithm"
+    algorithms$algorithm <- as.character(algorithms$algorithm)
+    
+    # create short names
+    for (i in 1:numOfAlgs) {
+        algFullName <- as.character(algorithms[i, 1])
+        algorithms[i, 1] <- shortNameAlgorithm(algFullName)
+    }
+    algorithms <- data.frame(t(algorithms))
+    
+    # change from factor to character
+    algorithms[] <- lapply(algorithms, as.character)
+    
+    # replace long names with short names
+    colnames(hugeMatrix) <- algorithms[1,]
+    
+    # calculate the number of all sets in files
+    quantityDataSets <- length(separate_evaluations)
+    
+    for (i in 1:quantityDataSets) {
+        # make a copy of set
+        dataSet <- as.data.frame(separate_evaluations[i])
+        
+        processedDataSet <- originalScoreData(hugeMatrix, dataSet)
+        
+        # add results to the matrix
+        hugeMatrix <- rbind(hugeMatrix, processedDataSet)
+    }
+    return (hugeMatrix)
+}
+
+originalScoreData <- function(dataMatrix, dataSet) { 
+    ## standard version with the original frulgality score 
+    
+    # receive name
+    xName <- as.character(dataSet[1, 2]) 
+    
+    cleanData <- computeTimeAUC(dataSet)
+    
+    # create a vector of values for this set
+    processedValues <- t(data.frame(rep(NA, numOfAlgs)))
+    colnames(processedValues) <- colnames(dataMatrix)
+    rownames(processedValues) <- xName
+    
+    # update value for each algorithm and data set
+    for (algValue in 1:nrow(cleanData)) {
+        algName <- as.character(cleanData[algValue, 3])
+        algIndex <-
+            grep(
+                pattern = substr(algName, 0, 5), x = colnames(processedValues), ignore.case = TRUE
+            )
+        processedValues[1, algIndex] <-
+            cleanData[algValue, 1] - 0.1 * log(1 + cleanData[algValue, 2])
+    }
+    
+    if (sum(is.na(processedValues) > 0)) {
+        
+    }
+    
+    # replace all NA values with negative value
+    processedValues[is.na(processedValues)] <- -1
+    
+    return (processedValues)
+}
+
+computeTimeAUC <- function(dataSet) {
+    dataSet <-
+        setNames(
+            dataSet, c(
+                "task_id", "dataset", "algo", "error_message", "accuracy", "auroc", "training_millis", "testing_millis", "confusion_matrix"
+            )
+        )
+    
+    # change type from factor to numeric and remove NA values
+    dataSet$auroc <- as.numeric(as.character(dataSet$auroc))
+    dataSet <- dataSet[!is.na(dataSet$auroc),]
+    
+    # store highest value for AUC in this particular data set
+    highestAUC <- max(dataSet$auroc)
+    
+    # convert time variable to numbers
+    dataSet$training_millis <-
+        as.numeric(as.character(dataSet$training_millis))
+    dataSet$testing_millis <-
+        as.numeric(as.character(dataSet$testing_millis))
+    
+    # check for missing or negative values
+    dataSet <- dataSet[!is.na(dataSet$training_millis),]
+    dataSet$training_millis <- abs(dataSet$training_millis)
+    
+    dataSet <- dataSet[!is.na(dataSet$testing_millis),]
+    dataSet$testing_millis <- abs(dataSet$testing_millis)
+    
+    # caclulate combined time for each algorithm
+    dataSet$combineTime <-
+        dataSet$training_millis + dataSet$testing_millis
+    
+    # find the lowest combined time in this data set
+    lowestTime <- min(dataSet$combineTime + 1)
+    
+    # filter columns and save AUC, training time and name of algorithm
+    dataSet <-
+        data.frame(dataSet$auroc, dataSet$combineTime, dataSet$algo)
+    smallX <-
+        setNames(dataSet, c("AUC", "CombineTime", "Algorithm"))
+    smallX <-
+        smallX[order(smallX$AUC,smallX$CombineTime,decreasing = TRUE),]
+    
+    return (smallX)
+}
+
+shortNameAlgorithm <- function(algOriginalName) {
+    posOfSpace <- gregexpr(pattern = ' ', algOriginalName)[[1]][1]
+    
+    if (posOfSpace > 0) {
+        shortName <-
+            paste(
+                substr(algOriginalName, 0, 5), substr(algOriginalName, 23, posOfSpace - 1), sep = ""
+            )
+    } else {
+        shortName <-
+            paste(substr(algOriginalName, 0, 5), substr(algOriginalName, 23, nchar(algOriginalName)), sep = "")
+    }
+    
+    return (shortName)
+}
